2ade6e74917aeb23d9921c0cc4dd7e9cfbbb70c3
haoyong <yong.hao@turingsenseai.com>
2021-02-25 19:21:42 +0800

    ✨feat: 连接mongo

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,38 @@
 import express, { Request, Response, Application } from 'express'
 import minimist from 'minimist'
-import { applicationStartMessage } from './shared/util'
+import mongoose, { ConnectOptions } from 'mongoose'
+import { applicationStartMessage, mongodbLinkPath } from './shared/util'
 
+/** app 创建 */
 const app: Application = express()
-
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello Ts Express')
+  res.send('my admin server')
 })
 
 /** 生产环境端口 */
 const productionPort: number = 3030
 const args = minimist(process.argv.slice(2))
 const { port: devPort } = args
-const port: number = devPort || 3030 // 端口号
-console.log('args,', args)
+const port: number = devPort || productionPort // 端口号
 process.argv.forEach((val, index) => {
   console.log(`${index}: ${val}`)
 })
 
-// const port: number = process.env
+/** 数据库连接 应用启动 */
+const mOpts:ConnectOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+}
+mongoose.connect(mongodbLinkPath, mOpts)
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'mongodb connection error:'))
+db.once('open', function() {
+  console.log("mongo链接OK")
 
-app.listen(port, ()=> {
-  console.log(applicationStartMessage)
-  console.log(`Started at port ${port}`)
+  app.listen(port, ()=> {
+    console.log(applicationStartMessage)
+    console.log(`Started at port ${port}`)
+  })
 })
